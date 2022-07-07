c28ddab81365b29271a746a71aa9016b79903ed9
DeemasDee <deemasdee@gmail.com>
2022-07-07 16:25:17 +0700

    upload flutter mobile homecook

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+//this file for setting primary color
+
+const kPrimaryColor = Color.fromARGB(255, 233, 155, 11);
+const kPrimaryLightColor = Color.fromRGBO(247, 216, 158, 1);
+
+const double defaultPadding = 16.0;
